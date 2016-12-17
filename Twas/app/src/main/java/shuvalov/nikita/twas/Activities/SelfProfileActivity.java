54ab8fdaff810d96f7033202d450d5f3146f88d5
NikShuvalov <nshuvalov89@gmail.com>
2016-12-17 16:04:29 -0500

    Added functionality to let user take selfie for profile picture. Profile pictures are now uploaded to firebase storage

--- Twas/app/src/main/java/shuvalov/nikita/twas/Activities/SelfProfileActivity.java
+++ Twas/app/src/main/java/shuvalov/nikita/twas/Activities/SelfProfileActivity.java
@@ -1,9 +1,11 @@
 package shuvalov.nikita.twas.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +16,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import shuvalov.nikita.twas.AppConstants;
+import shuvalov.nikita.twas.Helpers_Managers.NearbyManager;
 import shuvalov.nikita.twas.R;
 import shuvalov.nikita.twas.SelfProfileRecyclerAdapter;
 
@@ -25,6 +36,7 @@ public class SelfProfileActivity extends AppCompatActivity {
     Button mAccessGallery, mTakeSelfie;
 
     public static final int PICK_IMAGE_REQUEST = 1;
+    public static final int TAKE_IMAGE_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +60,14 @@ public class SelfProfileActivity extends AppCompatActivity {
             public void onClick(View view) {
                 switch(view.getId()){
                     case R.id.upload_image_gallery:
-                        Intent intent = new Intent();
-                        intent.setType("image/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+                        Intent galleryIntent = new Intent();
+                        galleryIntent.setType("image/*");
+                        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(galleryIntent,"Select Picture"),PICK_IMAGE_REQUEST);
                         break;
                     case R.id.selfie_button:
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent,TAKE_IMAGE_REQUEST);
                         break;
                     default:
                         Toast.makeText(SelfProfileActivity.this, "Gosh Darn't it", Toast.LENGTH_SHORT).show();
@@ -68,10 +82,35 @@ public class SelfProfileActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+        if(requestCode==PICK_IMAGE_REQUEST||requestCode==TAKE_IMAGE_REQUEST && resultCode==RESULT_OK && data!=null && data.getData()!=null){
             Uri uri = data.getData();
             try {
-                mProfileImage.setImageBitmap(MediaStore.Images.Media.getBitmap(getContentResolver(),uri));
+                Bitmap chosenProfileImage = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                mProfileImage.setImageBitmap(chosenProfileImage);
+
+                FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+                StorageReference bucketRef = firebaseStorage.getReferenceFromUrl(AppConstants.FIREBASE_IMAGE_BUCKET);
+
+                String id = NearbyManager.getInstance().getId();
+                StorageReference storageRef = bucketRef.child(String.format(AppConstants.FIREBASE_USER_PROFILE_IMAGE, id));
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                chosenProfileImage.compress(Bitmap.CompressFormat.JPEG,100, byteArrayOutputStream);
+                byte[] imgStream = byteArrayOutputStream.toByteArray();
+
+                UploadTask uploadTask = storageRef.putBytes(imgStream);
+                uploadTask.addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SelfProfileActivity.this, "Failed to upload picture", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(SelfProfileActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        //ToDo: Store DownloadURL, in FBDB as Profile parameter and in local SQL database.
+                    }
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
